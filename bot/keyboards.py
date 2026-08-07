9c9b1516d634401123f082a287a0d54a4013d428
AceHardwareGOK <armstein040803@gmail.com>
2026-08-07 05:12:04 +0300

    refactor: remove inline WebApp button from chat reply keyboard

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -10,17 +10,15 @@ MAIN_KEYBOARD = [
 
 def get_main_keyboard() -> ReplyKeyboardMarkup:
     """Build main reply keyboard."""
-    webapp_url = os.getenv("WEBAPP_URL") or WEBAPP_URL
     keyboard = [
         ['➕ Створити нагадування'],
         ['📋 Мої нагадування']
     ]
-    if webapp_url:
-        keyboard.insert(0, [KeyboardButton('📱 Відкрити Web App', web_app=WebAppInfo(url=webapp_url))])
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 
 MAIN_MARKUP = get_main_keyboard()
+
 
 
 
